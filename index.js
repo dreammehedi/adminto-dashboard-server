3cc76f975e34650f350f260e8dc9fd8ba48fa711
dreammehedi <dreammehedihassan@gmail.com>
2024-08-28 07:47:04 +0600

    Server Running Success

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv';
-import { connectDb } from './src/db/db.js';
-import { app } from './src/app.js';
+import dotenv from "dotenv";
+import { app } from "./src/app.js";
+import { connectDb } from "./src/db/db.js";
 
 dotenv.config({
-  path: './.env',
+  path: "./.env",
 });
 
 connectDb()
   .then(() => {
-    app.on('error', (error) => {
-      console.log('Server Initialization error', error);
+    app.on("error", (error) => {
+      console.log("Server Initialization error", error);
       throw error;
     });
-    app.get('/', (req, res) => res.send('Adminto Dashboard server'));
+    app.get("/", (req, res) => res.send("Adminto Dashboard server"));
     app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port:${process.env.PORT || 5000}`);
     });
   })
   .catch((error) => {
-    console.log('Database Server connection error', error);
+    console.log("Database Server connection error", error);
   });
